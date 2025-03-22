195457c74e47402d5d33a9f7280b623d92be1453
Artem <ionov96artem@mail.com>
2025-03-22 21:12:39 +0400

    Apply MySQL database django_weather

--- Weather/settings.py
+++ Weather/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "Weather.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",               # Настройка подключения к базе данных MySQL
+        "NAME": "django_weather",
+        "USER": "root",
+        "PASSWORD": "#lada2023_suPer",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
