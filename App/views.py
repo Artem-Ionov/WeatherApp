d6553d1df46c37b88dcfc66691ade8c69dd06c96
Artem <ionov96artem@mail.com>
2025-03-24 10:38:28 +0400

    Add comments and render in views, add base.html and town_weather.html

--- App/views.py
+++ App/views.py
@@ -1,54 +1,49 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 import requests
 
 def transfer_temperature(T):
+    "Перевод температуры из градусов Кельвина в градусы Цельсия"
     T = T - 273.15
     T = round(T, 2)
     return T
 
 def transfer_pressure(P):
+    "Перевод давления из гПа в мм рт. ст."
     P = P*100/133.3224
     P = round(P, 2)
     return P
 
 def town_weather(request):
+    "Получение информации о погоде в выбранном городе"
     city_name = 'Samara'
     country_code = 'RU'
     API_key = '5b65f99e785c9b22cca3eb29105347a9'
     lang = 'ru'
 
+    # Запрос, в который мы передаём название города и код страны, а получаем в ответе широту и долготу
     url1 = f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&limit=1&appid={API_key}'
     response1 = requests.get(url1)
-    response1 = response1.json()
-    lat = response1[0]['lat']                            # Широта
-    lon = response1[0]['lon']                            # Долгота
+    response1 = response1.json()                                        # Получаем ответ в формате JSON
+    town = response1[0]['name']                                         # Город
+    lat = response1[0]['lat']                                           # Широта
+    lon = response1[0]['lon']                                           # Долгота
 
+    # Запрос, в который мы передаём широту и долготу, а получаем в ответе информацию о погоде в данных координатах
     url2 = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_key}&lang={lang}'
     response2 = requests.get(url2)
-    response2 = response2.json()
-    state = response2['weather'][0]['description']
-    temp = response2['main']['temp']
+    response2 = response2.json()                                        # Получаем ответ в формате JSON
+    
+    state = response2['weather'][0]['description']                      # Общее погодное состояние
+    temp = response2['main']['temp']                                    # Температура
     temp = transfer_temperature(temp)
-    temp_feel = response2['main']['feels_like']
+    temp_feel = response2['main']['feels_like']                         # Температура по ощущениям
     temp_feel = transfer_temperature(temp_feel)
-    wind = response2['wind']['speed']
-    pressure = response2['main']['pressure']
+    wind = response2['wind']['speed']                                   # Скорость ветра
+    pressure = response2['main']['pressure']                            # Давление
     pressure = transfer_pressure(pressure)
-
-    return HttpResponse(f'''
-        Состояние погоды: {state}
-        Температура сейчас: {temp} C
-        Ощущается как: {temp_feel} C
-        Скорость ветра: {wind} м/с
-        Давление: {pressure} мм рт. ст.
-    ''')
-
-    print(response2)
-    print(f'Состояние погоды: {state}')
-    print(f'Температура сейчас: {temp} C')
-    print(f'Ощущается как: {temp_feel} C')
-    print(f'Скорость ветра: {wind} м/с')
-    print(f'Давление: {pressure} мм рт. ст.')
+    
+    # Данные, передаваемые в шаблон
+    content = {'town': town, 'state': state, 'temp': temp, 'temp_feel': temp_feel, 'wind': wind, 'pressure': pressure}
+    return render(request, 'town_weather.html', content)
 
 
